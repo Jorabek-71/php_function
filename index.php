b108d409a0ebf83e910e7de75814ad274c3f93a6
Jorabek-71 <botirovjorabek191@gmail.com>
2023-02-06 18:22:07 +0500

    baby

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+function name(){
+    print "jorabek";// one
+}
+function familya(){
+    return "botirov";//two
+}
+
+function otasiningismi(){
+    echo "odilbek og'li";//there
+}
+name(); //one
+
+echo familya(); //two
+
+otasiningismi(); //there
